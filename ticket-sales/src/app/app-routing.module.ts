f5cf4125e8941d5a7393433b24866953fd7350b3
Edwin Ramiro Garcia Chambilla <edwin.garcia@et.bo>
2024-11-21 23:24:10 -0400

    agrega dependencias importantes

--- ticket-sales/src/app/app-routing.module.ts
+++ ticket-sales/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  {
+    path: 'inicio',
+    loadComponent: () => import('./modulos/vista-principal/vista-principal.component')
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ticket-sales/src/app/modulos/vista-principal/vista-principal.component.ts
+++ ticket-sales/src/app/modulos/vista-principal/vista-principal.component.ts
@@ -1,10 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { PrimeModule } from 'src/app/prime.module';
 
 @Component({
   selector: 'app-vista-principal',
   templateUrl: './vista-principal.component.html',
-  styleUrls: ['./vista-principal.component.css']
+  styleUrls: ['./vista-principal.component.css'],
+  standalone: true,
+  imports: [RouterModule, PrimeModule, CommonModule],
 })
-export class VistaPrincipalComponent {
+export default class VistaPrincipalComponent {
 
 }
